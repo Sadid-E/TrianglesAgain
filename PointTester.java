95f66eae998bb496c88e8ccc54ac0f08017f7a1f
Sadid-E <sethun20@stuy.edu>
2020-10-29 21:10:12 -0400

    Added distanceTo and tests

---
+++ PointTester.java
@@ -0,0 +1,9 @@
+public class PointTester {
+
+  public static void main(String[] args) {
+    Point origin = new Point(0, 0);
+    Point other = new Point(3,5);
+    System.out.println(origin.distanceTo(other));
+  }
+
+}
