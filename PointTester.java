368d85a42484293f5559eb6a44271f4512a94119
Sadid-E <sethun20@stuy.edu>
2020-10-29 21:13:10 -0400

    Added equals method and tests

--- PointTester.java
+++ PointTester.java
@@ -3,7 +3,10 @@ public class PointTester {
   public static void main(String[] args) {
     Point origin = new Point(0, 0);
     Point other = new Point(3,5);
+    Point other2 = new Point(3,5);
     System.out.println(origin.distanceTo(other));
+    System.out.println(origin.equals(other));
+    System.out.println(other.equals(other2));
   }
 
 }
